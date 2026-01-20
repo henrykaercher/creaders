036570fd53df0ac711d439413d192a770e0033ef
Henry <henry.kaercher1@gmail.com>
2026-01-20 14:30:43 -0300

    markdown tests implementation

--- make.c
+++ make.c
@@ -7,6 +7,8 @@
 #define BUILD_FOLDER "build/"
 #define SRC_FOLDER "src/"
 
+#define TEST_FOLDER "tests/"
+
 int main(int argc, char *argv[]){
 	NOB_GO_REBUILD_URSELF(argc, argv);
 
@@ -16,19 +18,28 @@ int main(int argc, char *argv[]){
 
 #if !defined(_MSC_VER)
 	// On POSIX
-	nob_cmd_append(&cmd, "cc", "-Wall", "-std=c11", "-Wextra", "-o", BUILD_FOLDER"build", SRC_FOLDER"main.c");
+	if(argc > 1 && strcmp(argv[1], "mk_test") == 0) nob_cmd_append(&cmd, "cc", "-Wall", "-std=c11", "-Wextra", "-o", BUILD_FOLDER"build", TEST_FOLDER"mk_tests.c");
+	else nob_cmd_append(&cmd, "cc", "-Wall", "-std=c11", "-Wextra", "-o", BUILD_FOLDER"build", SRC_FOLDER"main.c");
 #else
 	// On MSVC
 	// this line its just hanging here, I dont really know MSCV, just keeping for the intention of an alternative
-	nob_cmd_append(&cmd, "cl", "/I.", "/std:c11", "/Fe:"BUILD_FOLDER"buid", SRC_FOLDER"main.c");
+	if(argc > 1 && strcmp(argv[1], "mk_test") == 0) nob_cmd_append(&cmd, "cl", "/I.", "/std:c11", "/Fe:"BUILD_FOLDER"buid", TEST_FOLDER"mk_tests.c");
+	else nob_cmd_append(&cmd, "cl", "/I.", "/std:c11", "/Fe:"BUILD_FOLDER"buid", SRC_FOLDER"main.c");
+
 #endif // _MSC_VER
 	
 	if(!nob_cmd_run(&cmd)) return 1;
 
 	nob_cc(&cmd);
     nob_cc_flags(&cmd);
-    nob_cc_output(&cmd, BUILD_FOLDER "mk_reader");
-    nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
+    if(argc > 1 && strcmp(argv[1], "mk_test") == 0){
+		nob_cc_output(&cmd, BUILD_FOLDER "mk_tests");
+		nob_cc_inputs(&cmd, TEST_FOLDER "mk_tests.c");
+	}
+	else{
+		nob_cc_output(&cmd, BUILD_FOLDER "mk_reader");
+		nob_cc_inputs(&cmd, SRC_FOLDER "main.c");
+	}
 	if (!nob_cmd_run(&cmd)) return 1;
 
     return 0;
